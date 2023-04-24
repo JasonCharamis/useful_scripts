67a129d84f8c6863f9a448d39b7f5b573a8e8763
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-04-24 17:06:42 +0300

    Update fasta_parser.pl

--- fasta_parser.pl
+++ fasta_parser.pl
@@ -1,45 +1,69 @@
-
-use strict;
+use strict;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 use warnings;
-
-open ( IN, $ARGV[0] );
 
 my @f = ();
 my %fasta=();
 my $seq=();
 my $id = ();
+my @filter=();
+my %filter=();
+my @inputs=();
 
-while ( my $line = <IN> ) {
 
+open ( IN, $ARGV[0] ) or die "USAGE: perl filter_fasta.pl 2-LINE_FASTA_FILE ID_TO_REMOVE]" ;
+open ( IN1, $ARGV[1]);
+
+
+sub FastaParser {
+
+    @inputs = @_;
+
+    my $line=$inputs[0];
     chomp $line;
-    
-    if ( $line =~ />/ ) {
-	$line =~ s/>//g;
-	$id = $line;
-	$fasta{$id}=1;
 
+    if ( $line =~ />/ ) {
+        $line =~ s/>| .*//g;
+        $id = $line;
     }
 
     elsif ( $line =~ /\--/ ) {
-	next;
+        next;
     }
 
     else {
-	$seq = $line;
-	$fasta{$id}=$line;
-    }
-   
-}
-
-
-foreach ( keys %fasta ) {
-
-    unless ( $_ =~ /jg|tet/ ) {
-	print ">$_\n$fasta{$_}\n";
+        $line =~ s/\*$|\.$//g;
+        $fasta{$id}=$line;
     }
 
 }
 
+
+#fasta hash, genes to remove                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+sub FilternPrint {
+    @filter=@_;
+    chomp $filter[0];
+    $filter{$filter[0]}=1;
+}
+
+
+while ( my $line = <IN> ) {
+    FastaParser ($line);
+}
+
+while ( my $line=<IN1> ) {
+    FilternPrint($line);
+}
+
+close (IN);
+
+
+my %seen=();
+
+for my $head ( keys %fasta ) {
+    unless ( exists $filter{$head} ) {
+        print ">","$head\n$fasta{$head}\n";
+    }
+}
 
 
 
