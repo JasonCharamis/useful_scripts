5c85493827ea2179df89b1eb23ca6980c0671850
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-04-29 14:52:18 +0300

    Update xlsx2tsv.py

--- xlsx2tsv.py
+++ xlsx2tsv.py
@@ -9,6 +9,6 @@ args = parser.parse_args()
 
 file=pd.read_excel(args.filename,  engine='openpyxl')
 
-out=re.sub(".xlsx",".csv",args.filename)
+out=re.sub(".xlsx",".tsv",args.filename)
 
 file.to_csv(out,sep="\t")
