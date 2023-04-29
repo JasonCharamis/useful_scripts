a02a697f6c2bb39a58d99f1c099c3820a0f3f458
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-04-29 14:52:04 +0300

    Create xlsx2tsv.py

---
+++ xlsx2tsv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
+
+file=pd.read_excel(args.filename,  engine='openpyxl')
+
+out=re.sub(".xlsx",".csv",args.filename)
+
+file.to_csv(out,sep="\t")
