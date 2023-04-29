11351a2b6df6579b8cab3b27b9aeb894bd280d2d
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-04-29 11:29:57 +0300

    Update extract_fasta.pl

--- extract_fasta.pl
+++ extract_fasta.pl
@@ -10,7 +10,7 @@ my %filter=();
 my @inputs=();
 
 
-open ( IN, $ARGV[0] ) or die "USAGE: perl filter_fasta.pl 2-LINE_FASTA_FILE [LIST_OF_IDs_TO_EXTRACT]" ;
+open ( IN, $ARGV[0] ) or die "USAGE: perl extract_fasta.pl 2-LINE_FASTA_FILE [LIST_OF_IDs_TO_EXTRACT]" ;
 open ( IN1, $ARGV[1]);
 
 
