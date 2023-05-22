9605ef46bc8175156b6177da3067f1765913c254
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-05-22 10:55:09 +0300

    Create midpoint_root.py

---
+++ midpoint_root.py
@@ -0,0 +1,24 @@
+import re
+import argparse
+from ete3 import Tree
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
+input = args.filename
+
+
+def midpoint_root(input):
+    tree = Tree(input, format = 1)
+    
+    ## get midpoint root of tree ##
+    midpoint = tree.get_midpoint_outgroup()
+
+    ## set midpoint root as outgroup ##
+    tree.set_outgroup(midpoint)
+    
+    ## write new file ##
+    tree.write(format=1, outfile=input+".midpoint_rooted")
+
+midpoint_root(input)
