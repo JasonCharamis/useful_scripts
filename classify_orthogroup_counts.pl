f43633efba4772b5599c70804bc0d5500176de42
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-06-01 10:55:14 +0300

    Add files via upload

---
+++ classify_orthogroup_counts.pl
@@ -0,0 +1,485 @@
+
+use strict;
+#use warnings;
+
+
+#scalars, arrays and hashes for parsing original file and counting number of species per OG
+my @f = ();
+my @count = ();
+my $number_of_cols = ();
+my %species_counter = ();
+
+#hashes for assigning OG id based on their presence
+my %culicidae = ();
+my %chironomidae = ();
+my %phlebotominae = ();
+my %phlebotomus = ();
+my %brachycera = ();
+my %lutzomyia = ();
+
+#assign OG categories
+my %universal = ();
+my %universal_single_copy = ();
+my %species_specific = ();
+my %phlebotomus_specific = ();
+my %lutzomyia_specific = ();
+my %chironomidae_wide = ();
+my %culicidae_wide = ();
+my %phlebotominae_wide = ();
+my %phlebotominae_patchy = ();
+my %nematocera_patchy = ();
+my %diptera_patchy = ();
+
+#scalars, arrays and arrays for parsing the file, assigning OG id with species and saving the contents into a hash
+my @file = ();
+my %file = ();
+my @h = ();
+my @j = ();
+my @y = ();
+my $hg = ();
+my %uniprint = ();
+
+#scalars, arrays and hashes for calculating the number of genes per category (universal, phlebotominae_wide etc) per species and printing results
+my %data = ();
+
+
+open ( IN, $ARGV[0] );
+
+open ( OUT0, ">orthology_results.tsv" );
+
+while ( my $line = <IN> ) {
+
+    push ( @file, $line);
+
+    chomp $line;
+    
+    #split file by columns
+    @f = split (/\t/,$line);
+
+    $number_of_cols = scalar(@f);
+
+    
+#first characterize orthogroups based on their presence (universal, widespread, phlebotominae, culicidae, chironomidae, nematocera, brachycera, none - note that this is per species )
+#I will use the orthogroup ids to infer number of genes per species in a second step
+
+    #create a variable to use as counter when finding one of the species
+    my $i = 0;
+
+    #skip orthogroup name and total (0 and 20)
+    for my $species (1..$number_of_cols-2) {
+	#count number of species with higher than zero number of genes per orthogroup
+	if ( $f[$species] > 0 ) {
+	    $i++;
+	    $species_counter{$f[0]} = $i;
+	    if ( $f[$species] == 1 ) {
+		$file{$f[0]}{$species} = $f[$species];
+	    }
+	}
+    }
+
+
+    my $p = 0;
+    #OGs present in phlebotominae with maximum species counts
+    for my $phlebotomines ( 9..$number_of_cols-2) {
+	if ( $f[$phlebotomines] > 0 ) {
+	    $p++;
+	    $phlebotominae{$f[0]} = $p;
+	}
+    }
+
+    my $ph = 0;
+    #OGs present in phlebotominae with maximum species counts
+    for my $phlebotomus ( 9..11, 14..16, 18, 19) {
+	if ( $f[$phlebotomus] > 0 ) {
+	    $ph++;
+	    $phlebotomus{$f[0]} = $ph;
+	}
+    }
+
+    my $ltz = 0;
+    #OGs present in phlebotominae with maximum species counts
+    for my $lutzomyia ( 12, 13) {
+	if ( $f[$lutzomyia] > 0 ) {
+	    $ltz++;
+	    $lutzomyia{$f[0]} = $ltz;
+	}
+    }
+
+    my $m = 0;
+    for my $mosquitoes (1,2,5) {
+	if ( $f[$mosquitoes] > 0 ) {
+	    $m++;
+	    $culicidae{$f[0]} = $m;
+	}
+    }
+
+    my $c = 0;
+    for my $midges (3,4,8) {
+	if ( $f[$midges] > 0 ) {
+	    $c++;
+	    $chironomidae{$f[0]} = $c;
+	}
+    }
+
+    my $d = 0;
+    for my $flies (6,7) {
+	if ( $f[$flies] > 0 ) {
+	    $d++;
+	    $brachycera{$f[0]} = $d;
+	}
+    }
+}
+
+
+#for the common keys, it automatically keeps the last (largest) values (don't know why)
+
+#get universal single copy
+my $universal_single_copy = 0;
+my $og = ();
+
+    
+for $og ( keys %file ) {
+    $universal_single_copy = 0;
+    foreach ( keys %{ $file{$og} } ) {
+	$universal_single_copy += $file{$og}{$_} ;
+    }
+
+    if ( $universal_single_copy ==  ( $number_of_cols-2 ) ) {
+	$universal_single_copy{$og} = 1;
+    }
+}
+
+ 
+#if orthogroup is found in all or all-but-two species save the ids into the universal dictionary    
+foreach ( sort keys %species_counter) {  
+    if ( $species_counter{$_} >= $number_of_cols-4 ) {
+	unless ( exists $universal_single_copy{$_} ) {
+	    $universal{$_} = $species_counter{$_};
+	}
+    }
+    
+    elsif ( $species_counter{$_} < 2 ) {
+	$species_specific{$_} = $species_counter{$_};
+    }
+}
+
+#orthogroups widespread (all or all-but-two) in phlebotominae    
+foreach ( sort keys %phlebotominae ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $chironomidae{$_} ) {
+			unless ( exists $culicidae{$_} ) {
+			    if ( $phlebotominae{$_} >= 9 ) {
+				$phlebotominae_wide{$_} = $phlebotominae{$_};
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+
+#orthogroups specific (all or all-but-one) to phlebotomus genus 
+foreach ( sort keys %phlebotomus ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $chironomidae{$_} ) {
+			unless ( exists $culicidae{$_} ) {
+			    unless ( exists $phlebotominae_wide{$_} ) {
+				unless ( exists $lutzomyia{$_} ) {
+				    if ( $phlebotomus{$_} >= 7 ) {
+					$phlebotomus_specific{$_} = $phlebotomus{$_};
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+#orthogroups specific (all) to lutzomyia genus 
+foreach ( sort keys %lutzomyia ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $chironomidae{$_} ) {
+			unless ( exists $culicidae{$_} ) {
+			    unless ( exists $phlebotominae_wide{$_} ) {
+				unless ( exists $phlebotomus{$_} ) {
+				    if ( $lutzomyia{$_} == 2 ) {
+					$lutzomyia_specific{$_} = $lutzomyia{$_};
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+#orthogroups present only in phlebotomines, and with patchy distribution
+foreach ( sort keys %phlebotominae ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $chironomidae{$_} ) {
+			unless ( exists $culicidae{$_} ) {
+			    unless ( exists $phlebotominae_wide{$_} ) {
+				unless ( exists $phlebotomus_specific{$_} ) {
+				    unless ( exists $lutzomyia_specific{$_} ) {				
+					if ( $phlebotominae{$_} >= 2 && $phlebotominae{$_} <= 8 ) {
+					    $phlebotominae_patchy{$_} = $phlebotominae{$_};
+					}
+				    }
+				}		
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+#orthogroups present in nematocera (phlebotomines and mosquitoes and midges), with patchy distribution
+foreach ( sort keys %phlebotominae ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $phlebotominae_wide{$_} ) {
+			unless ( exists $phlebotominae_patchy{$_} ) {
+			    unless ( exists $phlebotomus_specific{$_} ) {
+				unless ( exists $lutzomyia_specific{$_} ) {
+				    if ( $species_counter{$_} >= 2  && $species_counter{$_} < 17 ) {
+					$nematocera_patchy{$_} = $phlebotominae{$_};
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+#orthogroups present in diptera, with patchy distribution
+foreach ( sort keys %phlebotominae ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $nematocera_patchy{$_} ) {
+		    unless ( exists $phlebotominae_wide{$_} ) {
+			unless ( exists $phlebotominae_patchy{$_} ) {
+			    unless ( exists $phlebotomus_specific{$_} ) {
+				unless ( exists $lutzomyia_specific{$_} ) {
+				    if (  $species_counter{$_} >= 2  && $species_counter{$_} < 19) {
+					$diptera_patchy{$_} = $phlebotominae{$_};
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+
+#orthogroups specific (all or all-but-one) to mosquitoes
+foreach ( sort keys %culicidae ) {
+    unless ( exists $universal_single_copy{$_} ) {
+	unless ( exists $universal{$_} ) {
+	    unless ( exists $species_specific{$_} ) {
+		unless ( exists $brachycera{$_} ) {
+		    unless ( exists $phlebotominae{$_} ) {
+			unless ( exists $chironomidae{$_} ) {
+			    if ( $culicidae{$_} >= 2 ) {
+				$culicidae_wide{$_} = $culicidae{$_};
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+
+
+#orthogroups specific (all or all-but-one) to midges
+foreach ( sort keys %chironomidae ) {
+    unless ( exists $universal{$_} ) {
+	unless ( exists $species_specific{$_} ) {
+	    unless ( exists $brachycera{$_} ) {
+		unless ( exists $phlebotominae{$_} ) {
+		    unless ( exists $culicidae{$_} ) {
+			if ( $chironomidae{$_} >= 2 ) {
+			    $chironomidae_wide{$_} = $chironomidae{$_};
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+
+#now that i have the OG ids per category, I have to parse the file and print the number of genes per orthogroup per species
+for my $nl (0..scalar(@file)-1 )   {
+
+    #split file line-by-line and keep first line which contains species names
+    @j = split (/\n/,$file[0]);
+
+    #create an array with the species names 
+    @y = split (/\t/,$j[0] );
+
+    if ( $nl == 0 ) {next };
+    
+    @h = split (/\t/,$file[$nl]);
+    
+    #combine orthogroup name with species to parse the file
+    #this is based on the fact that the sequence of the h array which loops through the columns of the file per line 
+    #will have the same sequence with the y array which loops through the columns of the file only for the first line
+    #I have two independent arrays which loop through the columns
+    #the first has the species names, while the second takes as input the number of genes, while every line is an OG
+    #OG and species information is associated with the number of genes in the specific orthogroup in a hash
+        for my $col (1..scalar( @h )-2 ) {
+	    $uniprint{"$y[$col]:$h[0]"} = $h[$col];
+	#    $species_names{$y[$col]} = 1;
+	 }
+
+}
+
+
+#the uniprint hash now contains all the numbers of genes per orthogroup
+#on a manner that is easy to parse and extract
+#i will now use the OG ids that I have previously kept for getting the number of genes per orthogroup for each category
+#next, the numbers should be added to get the total number of genes per category
+
+foreach ( sort keys %uniprint ) {
+    ( my $n ) = ( $_ =~ /\:(OG\d+)/ );
+    ( my $sp ) = ( $_ =~ /^(\w+)\:/ );
+    $data{$sp}{$n}=$uniprint{$_};
+   }
+
+print OUT0 "Species\tUniversal_Single_Copy\tUniversal\tPhlebotominae_wide\tPhlebotominae_patchy\tPhlebotomus_specific\tLutzomyia_specific\tNematocera_Patchy\tBrachycera_Patchy\tSpecies_specific\n";
+
+for my $sp ( sort keys %data ) {
+
+    #counting arrays and scalars should be initialized in every species, otherwise the gene numbers will keep adding
+    my @number_universal = ();
+    my @number_universal_single_copy = ();
+    my @number_phlw = ();
+    my @number_phl_patchy = ();
+    my @number_phls = ();
+    my @number_ltz = ();
+    my @number_nematocera_patchy = ();
+    my @number_diptera_patchy = ();
+    my @number_spsp = ();
+    my $sum_uni = 0;
+    my $sum_uni_single_copy = 0;
+    my $sum_phlw = 0;
+    my $sum_phl_patchy = 0;
+    my $sum_phls = 0;
+    my $sum_ltz = 0;
+    my $sum_nematocera_patchy = 0;
+    my $sum_diptera_patchy = 0;
+    my $sum_spsp = 0;
+
+    #get number of genes per OG per species 
+    for my $hg ( sort keys %{ $data{$sp} } ) {
+
+	if ( exists ( $universal_single_copy{$hg} ) ) {
+	    push ( @number_universal_single_copy, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $universal{$hg} ) ) {
+	    push ( @number_universal, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $phlebotominae_wide{$hg} ) ) {
+	    push ( @number_phlw, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $phlebotominae_patchy{$hg} ) ) {
+	    push ( @number_phl_patchy, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $nematocera_patchy{$hg} ) ) {
+	    push ( @number_nematocera_patchy, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $diptera_patchy{$hg} ) ) {
+	    push ( @number_diptera_patchy, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $phlebotomus_specific{$hg} ) ) {
+	    push ( @number_phls, $data{$sp}{$hg} );
+	}
+	
+	if ( exists ( $lutzomyia_specific{$hg} ) ) {
+	    push ( @number_ltz, $data{$sp}{$hg} );
+	}
+
+	if ( exists ( $species_specific{$hg} ) ) {
+	    push ( @number_spsp, $data{$sp}{$hg} );
+	}
+    }
+
+    #add number of genes per category per species
+    for my $num (0..scalar( @number_universal))  {
+	$sum_uni += $number_universal[$num];
+    }
+
+    for my $num (0..scalar( @number_universal_single_copy))  {
+	$sum_uni_single_copy += $number_universal_single_copy[$num];
+    }
+
+    for my $num (0..scalar( @number_phlw))  {
+	$sum_phlw += $number_phlw[$num];
+    }
+
+    for my $num (0..scalar( @number_phls))  {
+	$sum_phls += $number_phls[$num];
+    }
+
+    for my $num (0..scalar( @number_ltz))  {
+	$sum_ltz += $number_ltz[$num];
+    }
+
+    for my $num (0..scalar( @number_phl_patchy))  {
+	$sum_phl_patchy += $number_phl_patchy[$num];
+    }
+
+    for my $num (0..scalar( @number_nematocera_patchy))  {
+	$sum_nematocera_patchy += $number_nematocera_patchy[$num];
+    }
+
+    for my $num (0..scalar( @number_diptera_patchy))  {
+	$sum_diptera_patchy += $number_diptera_patchy[$num];
+    }
+ 
+    for my $num (0..scalar( @number_spsp))  {
+	$sum_spsp += $number_spsp[$num];
+    }
+
+    #output in tab-separated, human-readable format
+    print OUT0 "$sp\t$sum_uni_single_copy\t$sum_uni\t$sum_phlw\t$sum_phl_patchy\t$sum_phls\t$sum_ltz\t$sum_nematocera_patchy\t$sum_diptera_patchy\t$sum_spsp\n";
+
+
+}
