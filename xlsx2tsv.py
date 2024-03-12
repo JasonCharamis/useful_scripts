07300573b39150c3023f4d813eadbdb2d1d8cc4b
JasonCharamis <jason.charamis@gmail.com>
2024-03-12 12:53:30 +0200

    Code refactoring...

--- xlsx2tsv.py
+++ xlsx2tsv.py
@@ -1,13 +1,31 @@
 import pandas as pd
 import argparse
 import re
+import openpyxl
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename')
+parser.add_argument('--spreadsheet', type = str, help = 'Input spreasheet file.')
 args = parser.parse_args()
 
-if args.filename is not None:
-    
-    file=pd.read_excel(args.filename, engine='openpyxl')
-    out=re.sub(".xlsx",".tsv",args.filename)
-    file.to_csv(out,sep="\t", index=False)
+
+def get_sheet_names(file):
+    try:
+        workbook = openpyxl.load_workbook(file)
+        sheet_names = workbook.sheetnames
+
+        return sheet_names
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+sheet_names = get_sheet_names ( args.spreadsheet )
+
+for sheet in sheet_names:
+    data_xlsx = pd.read_excel(args.spreadsheet, sheet, index_col=None)
+    data_xlsx.columns = [c.replace(' ', '_') for c in data_xlsx.columns]
+    df = data_xlsx.replace('\n', ' ',regex=True)
+
+    transposed_data = df.T
+    transposed_data.to_excel(f"{sheet}.xlsx", index=False)
